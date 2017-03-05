bcb69ee166438b4fcb15a3dded97d101666a0051
Alexis Schad <schadoc_alex@hotmail.fr>
2017-03-05 01:34:14 +0100

    first commit

---
+++ decoder.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const _ = require("lodash");
+
+var file = fs.readFileSync("A.txt", "utf-8").toString().split("\n");
+
+file.shift();
+var [W, H, N] = file.shift().split(" ").map(_.toNumber)
+
+console.log(W, H, N);
