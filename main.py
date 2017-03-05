eb9e74139979946bd52e05f2b1eb53f1e82178f3
Alexis Schad <schadoc_alex@hotmail.fr>
2017-03-05 22:13:50 +0100

    try to avoid ennemis

--- main.py
+++ main.py
@@ -22,6 +22,26 @@ reverse = {
     "D": "C",
     "B": "B"
 }
+offsetX = {
+    "E": -1,
+    "A": 1,
+    "C": 0,
+    "D": 0,
+    "B": 0
+}
+offsetY = {
+    "E": 0,
+    "A": 0,
+    "C": -1,
+    "D": 1,
+    "B": 0
+}
+
+def ennemiAdjacent(pos, ennemis):
+    for e in ennemis:
+        if abs(e[0] - pos[0]) + abs(e[1] - pos[1]) <= 1:
+            return True
+    return False
 
 # game loop
 while True:
@@ -30,9 +50,12 @@ while True:
     third_input = input()
     fourth_input = input()
     debug("%s %s %s %s" % (first_input, second_input, third_input, fourth_input))
-    for i in range(third_init_input):
-        fifth_input, sixth_input = [int(j) for j in input().split()]
-        debug("%d %d" % (fifth_input, sixth_input))
+    ennemis = []
+    for i in range(third_init_input - 1):
+        ennemis.append([int(j) for j in input().split()])
+        debug("%d %d" % (ennemis[-1][0], ennemis[-1][1]))
+    player = [int(j) for j in input().split()]
+    debug("%d %d" % (player[0], player[1]))
 
     # Write an action using print
     p = []
@@ -54,6 +77,13 @@ while True:
             p.append("D")
         if fourth_input == "_":
             p.append("E")
+
+    for c in p:
+        if ennemiAdjacent((player[0] + offsetX[c], player[1] + offsetY[c]), ennemis):
+            p.remove(c)
+
+    if len(p) == 0:
+        p.append("B")
 
     last_choice = p[random.randrange(len(p))]
 
